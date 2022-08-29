9b5a80ddb99ce7e4871fa3ecd75cb8a1ab5007b3
Администратор <george2066@yandex.ru>
2022-08-29 18:38:02 +0300

    Седьмое домашнее задание.

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,32 @@ public class Main {
         String Name2 = "ivan";
         String Name3 = "ivanovich";
         String fullName = Name1 + " " + Name2 + " " + Name3;
-        System.out.println(fullName);
+        System.out.println("ФИО сотрудника - " + fullName);
         System.out.println();
 
 
 
         /*ВТОРОЙ ТАСК*/
 
-        String[] fullNameArrey = fullName.split(" ");
-        for (int i = 0; i <= fullNameArrey.length - 1; i++) {
-
+        String si = "";
+        si = si + fullName.substring(0, 1).toUpperCase();
+        for (int i = 1; i <= fullName.length() - 1; i++) {
+            if(" ".equals(fullName.substring(i - 1, i))) {
+                si = si + fullName.substring(i, i + 1).toUpperCase();
+            } else {
+                si = si + fullName.substring(i, i + 1);
+            }
         }
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + si);
+        System.out.println();
+        System.out.println();
+
+
+
+        /*ТРЕТИЙ ТАСК*/
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
